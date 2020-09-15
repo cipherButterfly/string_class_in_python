497af6a8177e662bc3cc61c5a6e9b73fe434f4e7
repl.it user <replituser@example.com>
2020-09-15 09:26:03 +0000

    Second commit

--- classStringPython.py
+++ classStringPython.py
@@ -16,6 +16,22 @@ class String:
                 return char_position
                 break
             char_position = char_position + 1
+
+    def equals(self, string_2):
+      try:
+        i = 0
+        s = 0
+        while i < string_2.length():
+          if self.String[i] == string_2[i]:
+            s = s + 1
+        if s == string_2.length():
+          return True
+        else:
+          return False
+      except:
+        return False
+    
+
      
      
 # The below is just an example to run through all the functions of the String class
@@ -23,7 +39,11 @@ while true:
     print('Enter String')
     string_entered = input()
     final_string = String(string_entered)
-    print('Length(l) or character_position(cp)?')
+    print('''
+    Length(l)
+    character_position(cp)
+    equals(eq)
+    ''')
     choice_entered = input()
     if choice_entered == "l":
         print(str(final_string.length()))
@@ -31,6 +51,10 @@ while true:
         print("Enter the character")
         char_character_entered = input()
         print(str(final_string.char_position(char_character_entered)))
+    elif choice_entered == "eq":
+      string_1 = input()
+      string_2 = input()
+      print(string_1.equals(string_2))
     else:
         print("Invalid input, try again")
   
